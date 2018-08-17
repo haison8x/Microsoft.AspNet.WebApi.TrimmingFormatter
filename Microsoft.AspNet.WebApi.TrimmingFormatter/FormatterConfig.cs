d6bba0a015acd0f354127b369b202510ba4f1f5a
jacob.phan <jacob.phan@starixsoft.com>
2018-08-17 13:44:15 +0700

    Main

---
+++ Microsoft.AspNet.WebApi.TrimmingFormatter/FormatterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace Microsoft.AspNet.WebApi.TrimmingFormatter
+{
+    public static class FormatterConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Formatters.JsonFormatter.SerializerSettings.Converters
+                .Add(new TrimmingConverter());
+        }
+    }
+}
